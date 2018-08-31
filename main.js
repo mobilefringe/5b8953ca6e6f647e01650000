9bd17ea5dbab71224e11cbd3d9bb664c604e1bdb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-31 16:16:15 -0400

    Centennial Mall 1.0  /main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         created: function ()  {
             this.loadData().then(response => {
                 this.$store.dispatch("setDataLoaded", true); 
-                this.property.default_logo_url = "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1529701995203/default_logo_600.png";               
+                this.property.default_logo_url = "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1535746524000/centennial_default-compressor.png";               
                 var i_banner = this.findRepoByName('Inside Page Banner');
                 if(i_banner !== null && i_banner !== undefined && i_banner.images.length > 0) {
                     this.inside_banner = i_banner.images[0];
