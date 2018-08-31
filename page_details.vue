82c30a7600a69b5e0c5617e21cf3439b36a29912
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-31 16:34:29 -0400

    Centennial Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -37,22 +37,6 @@
                 }
             },
             created() {
-                var temp_repo = this.findRepoByName('Directory Banner');
-                if(temp_repo !== null && temp_repo !== undefined) {
-                   var images = temp_repo.images;
-                   if (images !== null && images !== undefined) {
-                        this.pageBanner = images[0];
-                    } else {
-                        this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
-                        }
-                    }
-                } else {
-                    this.pageBanner = {
-                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
-                    }
-                }
-                
                 this.updateCurrentPage(this.id);
             },
             watch: {
@@ -72,14 +56,21 @@
                         var _this = this;
                         this.property.mm_host = this.property.mm_host.replace("http:", "");
                         this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + _this.id + ".json" }).then(function (response) {
-                            var temp_repo = _this.findRepoByName('Inside Page Banner').images;
-                            if(temp_repo != null) {
-                                _this.pageBanner = temp_repo[0];
+                            var temp_repo = this.findRepoByName('Directory Banner');
+                            if(temp_repo !== null && temp_repo !== undefined) {
+                               var images = temp_repo.images;
+                               if (images !== null && images !== undefined) {
+                                    this.pageBanner = images[0];
+                                } else {
+                                    this.pageBanner = {
+                                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                                    }
+                                }
                             } else {
-                                _this.pageBanner = {
+                                this.pageBanner = {
                                     "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                                 }
-                            }  
+                            } 
                             
                             _this.currentPage = response.data;
                             _this.dataLoaded = true;
