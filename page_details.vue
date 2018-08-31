fb61ad975b1ad3a1dcab15803de45eeb309b94b6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-31 16:35:51 -0400

    Centennial Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -67,7 +67,7 @@
                                     }
                                 }
                             } else {
-                                this.pageBanner = {
+                                _this.pageBanner = {
                                     "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                                 }
                             } 
