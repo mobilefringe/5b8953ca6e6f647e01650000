65a3c32092ef24e1463ca4a2c2f14902cc853f47
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-31 13:33:31 -0400

    Centennial Mall 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -90,15 +90,27 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Contact Us Banner')
-                    if(temp_repo != null) {
-                        var images = temp_repo.images
-                        this.pageBanner = images[0];
-                    } else {
-                        this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                    var temp_repo = this.findRepoByName('Contact Us Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       if (temp_repo !== null && temp_repo !== undefined) {
+                            this.pageBanner = temp_repo[0];
+                        } else {
+                            this.pageBanner = {
+                                "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                            }
                         }
                     }
+                    
+                    // var temp_repo = this.findRepoByName('Contact Us Banner')
+                    // if(temp_repo != null) {
+                    //     var images = temp_repo.images
+                    //     this.pageBanner = images[0];
+                    // } else {
+                    //     this.pageBanner = {
+                    //         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                    //     }
+                    // }
                     this.currentPage = response[0].data;
                     this.dataLoaded = true;
                 });
