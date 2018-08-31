97965fa4557afa883dfd236872966e6107cdbf8b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-31 13:32:19 -0400

    Centennial Mall 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -121,13 +121,27 @@
             },
             created(){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Directory Banner')
-                    if(temp_repo != null) {
-                        var images = temp_repo.images
-                        this.pageBanner = images[0];
-                    } else {
-                        this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                    // var temp_repo = this.findRepoByName('Directory Banner');
+                    // if(temp_repo != null) {
+                    //     var images = temp_repo.images
+                        
+                    //     this.pageBanner = images[0];
+                    // } else {
+                    //     this.pageBanner = {
+                    //         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                    //     }
+                    // }
+                    
+                    
+                    var temp_repo = this.findRepoByName('Directory Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       if (temp_repo !== null && temp_repo !== undefined) {
+                            this.pageBanner = temp_repo[0];
+                        } else {
+                            this.pageBanner = {
+                                "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                            }
                         }
                     }
                     
