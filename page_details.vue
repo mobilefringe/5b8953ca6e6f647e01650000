d67865fbb089c429a861e18508315670b25ac8f3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-04 11:11:41 -0400

    Centennial Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/jpeg/1531500156000/sidebanner4.jpg" alt="" />    
+                            <img class="img_max" src="https://via.placeholder.com/220x600" alt="" />    
                         </div>
                         <div class="details_col_9">
                             <div class="page_body" v-html="currentPage.body"></div>
