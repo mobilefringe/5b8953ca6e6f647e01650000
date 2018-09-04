0f9beee25b802e89caf5c8b914927e8f10bbea0a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-04 11:30:28 -0400

    Centennial Mall 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -82,10 +82,16 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Hours Banner')
-                    if(temp_repo != null) {
-                        var images = temp_repo.images
-                        this.pageBanner = images[0];
+                    var temp_repo = this.findRepoByName('Hours Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       var images = temp_repo.images;
+                       if (images !== null && images !== undefined) {
+                            this.pageBanner = images[0];
+                        } else {
+                            this.pageBanner = {
+                                "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
+                            }
+                        }
                     } else {
                         this.pageBanner = {
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
