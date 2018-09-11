0d450c968ced0d6cc616ac1af44d83bc43d7acde
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-11 18:20:57 -0400

    Centennial Mall 1.0  /search_results.vue

--- search_results.vue
+++ search_results.vue
@@ -30,7 +30,8 @@
                                         </div>    
                                     </div> 
                                     <div v-else>
-                                        <img class="result_logo" :src="result.store.store_front_url_abs"/>    
+                                        <img v-if="result.store" class="result_logo" :src="result.store.store_front_url_abs"/>
+                                        <img v-else-if="result.store_front_url_abs" class="result_logo" :src="result.store_front_url_abs"/>
                                     </div>
                                 </div>
                                 <div v-else class="col-md-3 store_details_image center-block">
