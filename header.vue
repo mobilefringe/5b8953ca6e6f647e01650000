87d75ad9e46f53b41ba5bd9af06eecb61d72c3db
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-11 14:18:01 -0400

    Centennial Mall 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -178,9 +178,23 @@
                 }
             },
             methods: {
-                changeLocale: function(val) {
-                    // this will update the data store, which in turn will trigger the watcher to update the locale in the system
-                    this.locale = val; 
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData", "events"),this.$store.dispatch("getData", "promotions"),this.$store.dispatch("getData", "jobs")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);    
+                    }
+                },
+                onOptionSelect(option) {
+                    console.log("option", option)
+                    this.$router.push({
+                        name: "search-results",
+                        query: { searchQuery: this.search_result },
+                        params: { results: option }
+                    });
+                    this.$nextTick(function() {
+                        this.search_result = "";
+                    });
                 },
                 getWindowWidth(event) {
                     this.windowWidth = window.innerWidth;
