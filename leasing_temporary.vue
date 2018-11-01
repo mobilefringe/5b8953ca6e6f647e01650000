ccf2bd428c02a018f32f1fed3c6170e5ffc0fd10
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-01 12:08:23 -0400

    Centennial Mall 1.0  /leasing_temporary.vue

--- leasing_temporary.vue
+++ leasing_temporary.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/jpeg/1531500156000/sidebanner4.jpg" alt="" />    
+                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/jpeg/1537544234000/SideBanner2.jpg alt="" />    
                         </div>
                         <div class="details_col_9">
         				    <h3 class="inside_page_header">Specialty Short Term Leasing</h3>
