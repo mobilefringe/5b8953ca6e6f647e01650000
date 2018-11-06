e21f09c8b44862f7cf0ee7184e4f280d1cb8865f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:22:51 -0500

    Centennial Mall 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -82,20 +82,14 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Jobs Banner');
-                    if(temp_repo !== null && temp_repo !== undefined) {
-                        var images = temp_repo.images;
-                        if (images !== null && images !== undefined) {
-                            this.pageBanner = images[0];
-                        } else {
-                            this.pageBanner = {
-                                "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
-                            }
-                        }
-                    } else {
-                        this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
-                        }
+                    if(temp_repo && temp_repo.images) {
+                    this.pageBanner = temp_repo.images[0];
+                }
+                else {
+                    this.pageBanner = {
+                        "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                     }
+                }
                     var temp_repo1 = this.findRepoByName('Jobs Side Banner');
                     if(temp_repo1  && temp_repo1.images) {
                         this.sideBanner = temp_repo1.images[0];
