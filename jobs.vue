24ef5c795565c4c354837d0cd886dd6fd8268b2f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:22:39 -0500

    Centennial Mall 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -96,7 +96,14 @@ define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                         }
                     }
-
+                    var temp_repo1 = this.findRepoByName('Jobs Side Banner');
+                if(temp_repo1  && temp_repo1.images) {
+                    this.sideBanner = temp_repo1.images[0];
+                } else {
+                    this.sideBanner = {
+                        "image_url": ""
+                    }
+                } 
                     this.dataLoaded = true;
                     
                 });
