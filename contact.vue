fba3fec7e4e06312c5129eb49fe54206b4626099
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:20:39 -0500

    Centennial Mall 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -106,7 +106,14 @@
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                         }
                     }
-
+                    var temp_repo1 = this.findRepoByName('Contact Side Banner');
+                    if(temp_repo1 && temp_repo1.images) {
+                        this.sideBanner = temp_repo1.images[0];
+                    } else {
+                        this.sideBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b915e966e6f6472b6290000/image/png/1531495616000/inside_banner.png"
+                        }
+                    }
                     this.currentPage = response[0].data;
                     this.dataLoaded = true;
                 });
