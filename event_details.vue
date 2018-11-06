0a0f5563774c114edcbf68b933143e928f8f1494
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:18:37 -0500

    Centennial Mall 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -11,7 +11,7 @@
                 <div class="main_container mobile_padding margin_30">
                     <div class="details_row">
                         <div class="details_col_3 hidden_phone">
-                            <img class="img_max" src="//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/jpeg/1537544234000/SideBanner2.jpg" alt="" />    
+                            <img class="img_max" :src="sideBanner.image_url" alt="" />    
                         </div>
                         <div class="details_col_9" v-if="currentEvent">
                             <router-link to="/events">
