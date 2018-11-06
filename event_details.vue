f476c1ebe5a53204d2490b7ec76f34e6be94058c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:20:51 -0500

    Centennial Mall 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -80,7 +80,14 @@
                         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b8953ca6e6f647e01650000/image/png/1531495616000/inside_banner.png"
                     }
                 }
-                    
+                var temp_repo1 = this.findRepoByName('Events Side Banner');
+                if(temp_repo1 && temp_repo1.images) {
+                    this.sideBanner = temp_repo1.images[0];
+                } else {
+                    this.sideBanner = {
+                        "image_url": ""
+                    }
+                }     
 				this.$store.dispatch("getData", "events").then(response => {
 					this.currentEvent = this.findEventBySlug(this.id);
 					if (this.currentEvent === null || this.currentEvent === undefined) {
