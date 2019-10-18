444b81f989559450e595ff5d48b351620d24eee2
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-18 12:32:20 -0400

    Centennial Mall 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -118,7 +118,6 @@
                 reducedHolidays () {
                     var holidayHours = this.holidayHours;
                     return _.sortBy(_.filter(holidayHours, function(o) { return !o.is_closed; }), [function(o) { return o.holiday_date; }]);
-                    console.log(this.holidayHours)
                 },
                 closeHolidays () {
                     var holidayHours = this.holidayHours;
